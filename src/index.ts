7cf0eb38cf81255e24e030f6d34054f910602194
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-04 00:48:11 -0300

    feat: add bot listener in initBot function

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,20 @@ const token = process.env.DISCORD_TOKEN!;
 
 registerCommands();
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user!.tag}!`);
-});
+export async function initBot(): Promise<void> {
+    client.on("ready", () => {
+        console.log(`Logged in as ${client.user!.tag}!`);
+    });
+    
+    client.on("interactionCreate", async interaction => {
+        if (!interaction.isChatInputCommand()) return;
+    
+        if (interaction.commandName === "ping") {
+            await interaction.reply("Pong!");
+        }
+    });
+    
+    client.login(token);
+}
 
-client.on("interactionCreate", async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === "ping") {
-        await interaction.reply("Pong!");
-    }
-});
-
-client.login(token);
+initBot();
